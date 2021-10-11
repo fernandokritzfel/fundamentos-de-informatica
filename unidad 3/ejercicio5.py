273d1cbdb39e26b9b25de44f7f00fad0d1f4cd9a
fernando kritzfel <fernando.kritzfel@gmail.com>
2021-10-11 18:32:37 -0300

    nuevos ejercicios

--- unidad 3/ejercicio5.py
+++ unidad 3/ejercicio5.py
@@ -1,8 +1,8 @@
 
 
 def cuantos_dias(mes):
-    lista = [[105,"debe ingresar un numero mayor a 0"],["enero",31],["febrero", 28],["marzo",31],["abril",30],["mayo",31],["junio",30],["julio",31],["agosto",31],["septiembre",30],["octubre",31],["noviembre",30],["diciembre",31]]
-    dia = ("cantidad de dias del mes: ")+str(lista[mes][1])
+    lista = [["enero",31],["febrero", 28],["marzo",31],["abril",30],["mayo",31],["junio",30],["julio",31],["agosto",31],["septiembre",30],["octubre",31],["noviembre",30],["diciembre",31]]
+    dia = ("cantidad de dias del mes: ")+str(lista[mes-1][1])
     return dia
 
 
